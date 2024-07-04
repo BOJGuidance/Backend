5047714a018baf520b89b8d87ee3dca3a3e0d026
zestbear <zestbear90@gmail.com>
2024-07-04 22:11:57 +0900

    test: member test 클래스 생성

---
+++ src/test/java/com/boj/guidance/Member/MemberServiceTest.java
@@ -0,0 +1,8 @@
+package com.boj.guidance.Member;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberServiceTest {
+}
